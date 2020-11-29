a77b2f5beb78245c63c1aa1f0bbf0f09acdbb4a2
danimal141 <hideaki.ishii1204@gmail.com>
2020-11-29 18:13:49 +0900

    Fix constraint

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,8 +1,8 @@
 module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: true
-    field :description, String, null: true
+    field :title, String, null: false
+    field :description, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
